025837b381e269ef45ca58232618e019e278dcad
SergeTun <sergethurain2@gmail.com>
2025-05-08 21:51:38 +0200

    first commit

---
+++ src/components/button.jsx
@@ -0,0 +1,9 @@
+const Button = () => {
+    return (
+        <div className="flex justify-center items-center h-screen bg-gray-100">
+            <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded">
+            Click me
+            </button>
+        </div>
+    );
+}
